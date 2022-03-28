67662fdac6982024d87add19cdd8e2576540e2c5
Danilo Kodavara <danilo.kodavara@b2wdigital.com>
2022-03-28 20:11:03 -0300

    feat: create module to change rem of 3 and 5 to fizzbuzz

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,22 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FizzBuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read
   end
+
+  defp handle_file_read({:ok, result}) do
+    result
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&evaluate_number/1)
+  end
+
+  defp handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
 end
